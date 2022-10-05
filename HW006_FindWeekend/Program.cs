97aab7bf1f11126017bcaff4b3d3ac39a4ba9264
Nadezhda26m <nadezhda-26m@mail.ru>
2022-10-05 20:50:51 +0300

    add second solution

--- HW006_FindWeekend/Program.cs
+++ HW006_FindWeekend/Program.cs
@@ -7,32 +7,55 @@
 
 Console.Clear();
 
+// Console.WriteLine("Данная программа работает с цифрами, обозначающими день недели.");
+// Console.WriteLine("Введите цифру от 1 до 7, где 1 - понедельник, 7 - воскресенье, и нажмите Enter: ");
+// int day = int.Parse(Console.ReadLine());
+// int[] weekday = { 1, 2, 3, 4, 5, 6, 7 };
+// int week = weekday.Length;
+// int index = 0;
+// int dayInd = 0;
+// bool mistake = true;
+// while (index < week)
+// {
+//     if (weekday[index] == day)
+//     {
+//         mistake = false;
+//         dayInd = index;
+//     }
+//     index++;
+// }
+// if (mistake)
+// {
+//     Console.WriteLine("Вы ввели неверное значение");
+// }
+// else if (dayInd > 4)
+// {
+//     Console.WriteLine("Этот день является выходным");
+// }
+// else
+// {
+//     Console.WriteLine("Этот день не является выходным");
+// }
+
+#region Решение 2
+ 
 Console.WriteLine("Данная программа работает с цифрами, обозначающими день недели.");
 Console.WriteLine("Введите цифру от 1 до 7, где 1 - понедельник, 7 - воскресенье, и нажмите Enter: ");
 int day = int.Parse(Console.ReadLine());
-int[] weekday = {1, 2, 3, 4, 5, 6, 7};
-int week = weekday.Length;
-int index = 0;
-int dayInd = 0;
-bool mistake = true;
-while (index < week)
+if (day >= 1 && day <= 7)
 {
-    if (weekday[index] == day)
+    if (day == 6 || day == 7)
     {
-        mistake = false;
-        dayInd = index;
+        Console.WriteLine("Этот день является выходным");
     }
-    index++;
+    else
+    {
+        Console.WriteLine("Этот день не является выходным");
+    }
 }
-if (mistake)
+else
 {
     Console.WriteLine("Вы ввели неверное значение");
 }
-else if (dayInd > 4)
-{
-    Console.WriteLine("Этот день является выходным");
-}
-else 
-{
-    Console.WriteLine("Этот день не является выходным");
-}
+
+#endregion
