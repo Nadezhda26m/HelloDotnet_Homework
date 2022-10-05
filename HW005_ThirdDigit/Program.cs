834129205c94aeecabe10ae304b9564c3c140575
Nadezhda26m <nadezhda-26m@mail.ru>
2022-10-05 18:43:07 +0300

    add version from seminar

--- HW005_ThirdDigit/Program.cs
+++ HW005_ThirdDigit/Program.cs
@@ -10,7 +10,7 @@ Console.Clear();
 int number = new Random().Next(-999_999_999, 1_000_000_000);
 Console.WriteLine($"Задумали целое число {number}");
 int count = 0; // количество разрядов
-int num = number; 
+int num = number;
 while (num != 0) // ищем количество разрядов в числе
 {
     num = num / 10;
@@ -23,18 +23,26 @@ if (count < 3) // проверяем, есть ли третья цифра
 else // ищем третью цифру
 {
     num = number;
-    int amount = 0;
     int divisor = count - 2;
     int third = 0;
-    if (num < 0) // избавляемся от минуса
-    {
-        num = num * (-1);
-    }
-    while (amount < divisor) // сколько раз делить на 10
+    if (num < 0) num = -num; // избавляемся от минуса
+    for (int amount = 0; amount < divisor; amount++) // сколько раз делить на 10
     {
         third = num % 10;
         num = num / 10;
-        amount++;
     }
     Console.WriteLine($"Третья цифра числа: {third}");
 }
+
+// Решение с семинара
+// int a = new Random().Next(-999_999_999, 1_000_000_000);
+// Console.WriteLine($"Задумали целое число {a}");
+// if (a < 0) a = -a;
+// if (a >= 100)
+// {
+//     while (a > 999)
+//     {
+//         a = a / 10;
+//     }
+//     Console.WriteLine(a % 10);
+// }
