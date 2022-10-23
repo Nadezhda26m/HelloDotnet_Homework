cee0d09116966796d760fc67ecae97af7d1f0be8
Nadezhda26m <nadezhda-26m@mail.ru>
2022-10-23 15:22:23 +0300

    HW 022 add comments and UI

--- HW022_Print3DArray/Program.cs
+++ HW022_Print3DArray/Program.cs
@@ -14,17 +14,25 @@
 // 3. Распечатать массив построчно с указанием индексов
 
 // Методы
+// 1. Метод создания трехмерного массива
+// 2. Метод получения чисел из отрезка в случайном порядке 
+// без повтора
+// 2.1 Метод получения нескольких чисел из отрезка 
+// в случайном порядке без повтора
+// 3. Метод заполнения трехмерного массива числами
+// из одномерного массива
+// 4. Метод печати трехмерного массива в консоль по рядам
+// с указанием индексов каждого элемента
 
 #region Методы
-
-// создать трехмерный массив
+// 1. Метод создания трехмерного массива
 int[,,] CreateCube(int layer, int row, int col)
 {
     return new int[layer, row, col];
 }
 
-// получить неповторяющиеся числа
-
+// 2. Метод получения чисел из отрезка в случайном порядке 
+// без повтора
 int[] GetAllDifferentIntNumbers(int min, int max)
 {
     int size = Math.Abs(min - max) + 1;
@@ -46,7 +54,8 @@ int[] GetAllDifferentIntNumbers(int min, int max)
     return differentNums;
 }
 
-// 
+// 2.1 Метод получения нескольких чисел из отрезка 
+// в случайном порядке без повтора
 int[] GetSomeDifferentIntNumbers(int count, int min, int max)
 {
     int[] differentNums = new int[count];
@@ -67,7 +76,8 @@ int[] GetSomeDifferentIntNumbers(int count, int min, int max)
     return differentNums;
 }
 
-// заполнить
+// 3. Метод заполнения трехмерного массива числами
+// из одномерного массива
 void Fill3DArrayFrom(int[] numbers, int[,,] array3D)
 {
     int layer = array3D.GetLength(0);
@@ -86,7 +96,8 @@ void Fill3DArrayFrom(int[] numbers, int[,,] array3D)
     }
 }
 
-// 
+// 4. Метод печати трехмерного массива в консоль по рядам
+// с указанием индексов каждого элемента
 void Print3DArrayWithIndex(int[,,] array3D)
 {
     int layer = array3D.GetLength(0);
@@ -105,19 +116,20 @@ void Print3DArrayWithIndex(int[,,] array3D)
         Console.WriteLine();
     }
 }
-
-
 #endregion Методы
 
-
 Console.Clear();
-int[,,] cube = CreateCube(2, 3, 4);
+int[,,] cube = CreateCube(3, 4, 6);
 int layer = cube.GetLength(0);
 int row = cube.GetLength(1);
 int col = cube.GetLength(2);
 int countElements = layer * row * col;
 int rangeStart = 10;
-int rangeEnd = 33;
+int rangeEnd = 99;
+Console.WriteLine($"Задан трехмерный массив размером "
+                + $"{layer} x {row} x {col}");
+Console.WriteLine($"Данный массив нужно заполнить неповторяющимися "
+                + $"числами из отрезка [{rangeStart},{rangeEnd}]");
 if (countElements <= rangeEnd - rangeStart + 1)
 {
     int[] num = GetSomeDifferentIntNumbers
@@ -125,6 +137,14 @@ if (countElements <= rangeEnd - rangeStart + 1)
     // Console.WriteLine(String.Join(",", num));
     // Console.WriteLine();
     Fill3DArrayFrom(num, cube);
+    Console.WriteLine("Полученный массив (с указанием индексов "
+                    + "каждого элемента):");
     Print3DArrayWithIndex(cube);
 }
-else Console.WriteLine("Выбран неверный диапазон чисел");
+else 
+{
+    Console.WriteLine();
+    Console.WriteLine("Чисел из выбранного диапазона недостаточно "
+                    + "для заполнения заданного массива");
+    Console.WriteLine("Измените размер массива или диапазон чисел");
+}
