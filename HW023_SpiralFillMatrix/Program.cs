d79e65c9a38680770d9fa46318234d4124b6cd29
Nadezhda26m <nadezhda-26m@mail.ru>
2022-10-28 23:18:26 +0300

    HW 023 Fill spiral without flag, big method

--- HW023_SpiralFillMatrix/Program.cs
+++ HW023_SpiralFillMatrix/Program.cs
@@ -13,10 +13,9 @@
 
 // Методы
 // 1. Метод создания двумерного массива
-// 2.1. Метод спирального заполнения двумерного массива 
-// по часовой стрелке _с равным количеством строк и столбцов_ 
-// от элемента на позиции [0, 0] с начальным флагом 1
-// 2.2. _с разным количеством строк и столбцов_
+// 2.1. Метод спирального заполнения двумерного массива по часовой 
+// стрелке от элемента на позиции [0, 0] с начальным флагом 1
+// 2.2. без флага
 // 3. Метод для печати матрицы
 
 #region Методы
@@ -26,37 +25,8 @@ int[,] CreateMatrix(int row, int col)
     return new int[row, col];
 }
 
-// 2.1. Метод спирального заполнения двумерного массива 
-// по часовой стрелке _с равным количеством строк и столбцов_ 
-// от элемента на позиции [0, 0] с начальным флагом 1
-void FillSpiral(int[,] matr, int row, int num, int i, int j, int flag)
-{
-    if (row > 0)
-    {
-        int countSteps = 0;
-        if (flag == 5) flag = 1;
-        while (countSteps < row)
-        {
-            matr[i, j] = num;
-            num++;
-            countSteps++;
-            if (flag == 1) j++; // вправо
-            else if (flag == 2) i++; // вниз
-            else if (flag == 3) j--; // влево
-            else i--; // вверх
-        }
-        if (flag == 1)
-            FillSpiral(matr, row - 1, num, i + 1, j - 1, flag + 1);
-        else if (flag == 2)
-            FillSpiral(matr, row, num, i - 1, j - 1, flag + 1);
-        else if (flag == 3)
-            FillSpiral(matr, row - 1, num, i - 1, j + 1, flag + 1);
-        else
-            FillSpiral(matr, row, num, i + 1, j + 1, flag + 1);
-    }
-}
-
-// 2.2. _с разным количеством строк и столбцов_
+// 2.1. Метод спирального заполнения двумерного массива по часовой 
+// стрелке от элемента на позиции [0, 0] с начальным флагом 1
 void FillSpiral2(int[,] matr, int rowAll, int colAll,
                     int num, int i, int j, int flag)
 {
@@ -94,6 +64,79 @@ void FillSpiral2(int[,] matr, int rowAll, int colAll,
     }
 }
 
+// 2.2. Метод спирального заполнения двумерного массива 
+// по часовой стрелке от элемента на позиции [0, 0] 
+// // с начальным флагом 1
+void FillSpiral(int[,] matr, int row, int col, int num, int i, int j)
+{
+    if (row != 0 && col != 0)
+    {
+        int count = 0;
+        if (i == 0 && j == 0)
+        {
+            matr[i, j] = num;
+            count = 1;
+            num++;
+        }
+        if (j + 1 < matr.GetLength(1))
+        {
+            if (matr[i, j + 1] == 0)
+            {
+                while (count < col)
+                {
+                    j++; // вправо
+                    matr[i, j] = num;
+                    num++;
+                    count++;
+                }
+                FillSpiral(matr, row - 1, col, num, i, j);
+            }
+        }
+        if (i + 1 < matr.GetLength(0))
+        {
+            if (matr[i + 1, j] == 0)
+            {
+                while (count < row)
+                {
+                    i++; // вниз
+                    matr[i, j] = num;
+                    num++;
+                    count++;
+                }
+                FillSpiral(matr, row, col - 1, num, i, j);
+            }
+        }
+        if (j - 1 >= 0)
+        {
+            if (matr[i, j - 1] == 0)
+            {
+                while (count < col)
+                {
+                    j--; // влево
+                    matr[i, j] = num;
+                    num++;
+                    count++;
+                }
+                FillSpiral(matr, row - 1, col, num, i, j);
+            }
+        }
+        if (i - 1 >= 0)
+        {
+            if (matr[i - 1, j] == 0)
+            {
+                while (count < row)
+                {
+                    i--; // вверх
+                    matr[i, j] = num;
+                    num++;
+                    count++;
+                }
+                FillSpiral(matr, row, col - 1, num, i, j);
+            }
+        }
+    }
+}
+
 // 3. Метод для печати матрицы
 void PrintMatrix(int[,] matr)
 {
@@ -109,98 +152,19 @@ void PrintMatrix(int[,] matr)
 }
 #endregion Методы
 
-//? над удалением флага и магических чисел в аргументах еще думаю
-//? и попробую сделать заполнение не с позиции [0, 0]
-
 Console.Clear();
-int row4 = 4;
-int[,] matrix4 = CreateMatrix(row4, row4);
-int numberStart = 11;
-FillSpiral(matrix4, row4, numberStart, 0, 0, 1);
-Console.WriteLine($"Задана матрица размером {row4} x {row4}");
-Console.WriteLine($"Заполним ее по спирали целыми числами, начиная с {numberStart}:");
-PrintMatrix(matrix4);
-Console.WriteLine();
-
 int[,] matrix5 = CreateMatrix(4, 7);
 int row5 = matrix5.GetLength(0);
 int col5 = matrix5.GetLength(1);
+int numberStart = 11;
 FillSpiral2(matrix5, row5, col5, numberStart, 0, 0, 1);
 Console.WriteLine($"Пример спирального заполнения матрицы {row5} x {col5}:");
 PrintMatrix(matrix5);
 Console.WriteLine();
 
-
-// END
-
-
-
-
-
-
-
-// Метод спирального заполнения двумерного массива 
-// по часовой стрелке с разным количеством строк и столбцов 
-// от элемента на позиции [0, 0] 
-// // с начальным флагом 1
-// void FillSpiral3(int[,] matr, int row, int col, int num, int i, int j)
-// {
-//     if (row != 0 && col != 0)
-//     {
-//         int count = 0;
-//         int flag = 4;
-//         if (j + 1 < matr.GetLength(1))
-//         {
-//             if (matr[i, j + 1] == 0) flag = 1;
-//         }
-//         if (flag == 4 && i + 1 < matr.GetLength(0))
-//         {
-//             if (matr[i + 1, j] == 0) flag = 2;
-//         }
-//         if (flag == 4 && j - 1 >= 0)
-//         {
-//             if (matr[i, j - 1] == 0) flag = 3;
-//         }
-//         // else if (i - 1 >= 0)
-//         // {
-//         //     if (matr[i - 1, j] == 0) flag = 4;
-//         // }
-//         // if (flag == 5) flag = 1;
-//         if (flag % 2 == 0)
-//         {
-//             while (count < row)
-//             {
-//                 matr[i, j] = num;
-//                 num++;
-//                 count++;
-//                 if (flag == 2) i++; // вниз
-//                 else i--; // вверх
-//             }
-//         }
-//         else
-//         {
-//             while (count < col)
-//             {
-//                 matr[i, j] = num;
-//                 num++;
-//                 count++;
-//                 if (flag == 1) j++; // вправо
-//                 else j--; // влево
-//             }
-//         }
-//         if (flag == 1)
-//             FillSpiral3(matr, row - 1, col, num, i + 1, j - 1);
-//         if (flag == 2)
-//             FillSpiral3(matr, row, col - 1, num, i - 1, j - 1);
-//         if (flag == 3)
-//             FillSpiral3(matr, row - 1, col, num, i - 1, j + 1);
-//         if (flag == 4)
-//             FillSpiral3(matr, row, col - 1, num, i + 1, j + 1);
-//     }
-// }
-
-// int[,] matrix6 = CreateMatrix(6, 10);
-// int row6 = matrix6.GetLength(0);
-// int col6 = matrix6.GetLength(1);
-// FillSpiral3(matrix6, row6, col6, numberStart, 0, 0);
-// PrintMatrix(matrix6);
+// Решение без флага
+int[,] matrix6 = CreateMatrix(4, 7);
+int row6 = matrix6.GetLength(0);
+int col6 = matrix6.GetLength(1);
+FillSpiral(matrix6, row6, col6, numberStart, 0, 0);
+PrintMatrix(matrix6);
